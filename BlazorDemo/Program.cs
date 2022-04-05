1bc096bdc8ae9948e2cf0b07eba47bd7a9fd9a20
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-04-05 15:53:51 +0200

    Add project files.

---
+++ BlazorDemo/Program.cs
@@ -0,0 +1,12 @@
+using BlazorDemo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app"); //även i App.razor. Id=App i diven
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
